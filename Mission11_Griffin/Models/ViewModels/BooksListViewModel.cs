10840ae3de1705db03cce1f7fca6d6071a03d6ca
Charlotte Griffin <charlottegriffin11@gmail.com>
2024-03-19 23:24:47 -0600

    Add project files.

---
+++ Mission11_Griffin/Models/ViewModels/BooksListViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Mission11_Griffin.Models.ViewModels
+{
+    public class BooksListViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
+    }
+}
+
+
